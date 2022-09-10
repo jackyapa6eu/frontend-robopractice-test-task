836d9f55f0f0d54c2ff7c2a7637ea18c0b3bd5a6
Evgenii Denisov <jackyapa6eu@yandex.ru>
2022-09-10 14:41:14 +0300

    some refactor, getting data, antd installed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const port = 8080;
+const cors = require('cors');
+
+const whitelist = [
+  'http://localhost:3000'
+];
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS.'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 
 app.get('/api/users', (req, res) => {
   res.send(
